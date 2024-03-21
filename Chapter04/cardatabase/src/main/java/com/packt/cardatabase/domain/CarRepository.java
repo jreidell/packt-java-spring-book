0cb771308de591ec973c9989718d361173f41671
joe.reidell <joe@reidell.net>
2024-03-21 11:19:07 -0400

    implemented the OpenApi specification and integrated the swagger ui into the project

--- Chapter04/cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ Chapter04/cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,11 +1,10 @@
 package com.packt.cardatabase.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CarRepository extends CrudRepository<Car, Long>, PagingAndSortingRepository<Car, Long> {
     // Fetch cars by brand
@@ -15,27 +14,27 @@ public interface CarRepository extends CrudRepository<Car, Long>, PagingAndSorti
     List<Car> findByColor(@Param("color") String color);
 
     // Fetch cars by model
-    List<Car> findByModel(String model);
+    List<Car> findByModel(@Param("model") String model);
 
     // Fetch cars by model year
-    List<Car> findByModelYear(int modelYear);
+    List<Car> findByModelYear(@Param("modelyear") int modelYear);
 
     // Fetch cars by brand and model
-    List<Car> findByBrandAndModel(String brand, String model);
+    List<Car> findByBrandAndModel(@Param("brand") String brand, @Param("model") String model);
 
     // Fetch cars by brand and color
-    List<Car> findByBrandAndColor(String brand, String color);
+    List<Car> findByBrandAndColor(@Param("brand") String brand, @Param("color") String color);
 
     // Fetch cars by brand and sort by model year
-    List<Car> findByBrandOrderByModelYearAsc(String brand);
+    List<Car> findByBrandOrderByModelYearAsc(@Param("brand") String brand);
 
     // EXAMPLE OF SQL STATEMENTS USING THE @Query ANNOTATION
     // Fetch cars by brand using SQL
     //@Query("select c from Car c where c.brand = ?1")
-    //List<Car> findByBrand(String brand);
+    //List<Car> findByBrand(@PathParam("brand") String brand);
 
     // Fetch cars by brand using SQL
     @Query("select c from Car c where c.brand like %?1")
-    List<Car> findByBrandEndsWith(String brand);
+    List<Car> findByBrandEndsWith(@Param("brand") String brand);
 
 }
