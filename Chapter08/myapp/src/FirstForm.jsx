e105e5543101cda43353bf3435f46983b0880b14
joe.reidell <joe@reidell.net>
2024-03-25 22:27:41 -0400

    demonstrates using an object to store form value state

--- Chapter08/myapp/src/FirstForm.jsx
+++ Chapter08/myapp/src/FirstForm.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 
 function FirstForm() {
-    const [text, setText] = useState('');
+    const [user, setUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    });
 
     const handleChange = (event) => {
-        setText(event.target.value);
+        setUser({...user, [event.target.name]: event.target.value});
     }
 
     const handleSubmit = (event) => {
-        alert(`You typed: ${text}`);
+        alert(`Hello ${user.firstName} ${user.lastName}`);
         event.preventDefault();
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" onChange={event => setText(event.target.value)} value={text} />
+            <label>First Name </label>
+            <input type="text" name="firstName" onChange={handleChange} value={user.firstName} /><br />
+            <label>Last Name </label>
+            <input type="text" name="lastName" onChange={handleChange} value={user.lastName} /><br />
+            <label>Email </label>
+            <input type="email" name="email" onChange={handleChange} value={user.email} /><br />
             <input type="submit" value="Press me" />
         </form>
     );
