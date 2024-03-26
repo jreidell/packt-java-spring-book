7a6d67b1a5b8ca8196c186758f254f7485ebb1e0
joe.reidell <joe@reidell.net>
2024-03-26 14:39:23 -0400

    refactored the search and results to use the ag-grid component to display paged, sortable, filterable data in a manageable, styled table view

--- Chapter11/restgithub/src/App.tsx
+++ Chapter11/restgithub/src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { AgGridReact } from 'ag-grid-react';
+import { ColDef, ICellRendererParams } from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 import './App.css'
 
 type Repository = {
@@ -20,28 +24,37 @@ function App() {
       .catch(err => console.error(err));
   }
 
+  const [columnDefs] = useState<ColDef[]>([
+    {field: 'full_name', headerName: "Author", sortable: true, filter: true},
+    {field: 'owner.login', headerName: "Owenr Login", sortable: true, filter: true},
+    {field: 'html_url', headerName: "Repo URL", sortable: true, filter: true, cellRenderer: (params: ICellRendererParams) => (<a href={params.value} target="_blank" rel="noreferrer">{params.value}</a>)},
+    {
+      field: "id",
+      headerName: "",
+      cellRenderer: (params: ICellRendererParams) => (
+        <button onClick={() => alert(`GET '/api/users/${params.value}'`)}>Edit</button>
+      ),
+      sortable: false, 
+      filter: false
+    }
+  ]);
+
   return (
-    <>
+    <div className='App'>
       <input
         value={keyword}
         onChange={e => setKeyword(e.target.value)}
       />
       <button onClick={handleClick}>So Fetch</button>
-      {repodata.length === 0 ? (
-        <p>No data available</p>
-      ) : (
-        <table>
-          <tbody>
-            {repodata.map(repo => (
-              <tr key={repo.id}>
-                <td>{repo.full_name}</td>
-                <td><a href={repo.html_url}>{repo.html_url}</a></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
+      <div className='ag-theme-material' style={{height: 500, width: 850}}>
+        <AgGridReact 
+          rowData={repodata}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={8}
+        />
+      </div>
+    </div>
   )
 }
 
