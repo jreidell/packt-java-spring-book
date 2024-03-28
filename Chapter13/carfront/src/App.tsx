4f7f22cc3eec312a99e98b3dec986514bbd89547
joe.reidell <joe@reidell.net>
2024-03-28 15:36:52 -0400

    created chapter 13 folder and copied the carfront and cardatabase from chapter 12

---
+++ Chapter13/carfront/src/App.tsx
@@ -0,0 +1,18 @@
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from "@mui/material"
+
+function App() {
+  return (
+    <Container maxWidth="x1">
+      <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Car Shop
+            </Typography>
+          </Toolbar>
+        </AppBar>
+    </Container>
+  )
+}
+
+export default App
