cd99e7e2430ca22ece8f7d89d58d1efcc168b5de
joe.reidell <joe@reidell.net>
2024-03-28 21:05:59 -0400

    added delete functionality to the cars api and front end app

--- Chapter13/carfront/src/api/carapi.ts
+++ Chapter13/carfront/src/api/carapi.ts
@@ -5,3 +5,8 @@ export const getCars = async (): Promise<CarRepsonse[]> => {
     const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/cars`);
     return response.data._embedded.cars;
 }
+
+export const deleteCar = async (link: string): Promise<CarRepsonse> => {
+    const response = await axios.delete(link);
+    return response.data;
+}
--- Chapter13/carfront/src/components/CarList.tsx
+++ Chapter13/carfront/src/components/CarList.tsx
@@ -1,11 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCars } from "../api/carapi";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { deleteCar, getCars } from "../api/carapi";
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
 
 function CarList() {
+    const queryClient = new QueryClient();
+
     const { data, error, isSuccess } = useQuery({
         queryKey: ["cars"],
         queryFn: getCars,
+    });
+
+    const { mutate } = useMutation(deleteCar, {
+        onSuccess: () => {
+            // car deleted
+            console.log("sucksess!");
+            queryClient.invalidateQueries({ queryKey: ["cars"] });
+        },
+        onError: (err) => {
+            console.error(err);
+        }
     });
 
     const columns: GridColDef[] = [
@@ -14,7 +27,18 @@ function CarList() {
         {field: 'color', headerName: "Color", width: 200},
         {field: 'registrationNumber', headerName: "Reg.nr", width: 150},
         {field: 'modelYear', headerName: "Model Year", width: 150},
-        {field: 'price', headerName: "Price", width: 150}
+        {field: 'price', headerName: "Price", width: 150},
+        {
+            field: "delete",
+            headerName: "",
+            width: 90,
+            sortable: false,
+            filterable: false,
+            disableColumnMenu: true,
+            renderCell: (params: GridCellParams) => (
+                <button onClick={() => mutate(params.row._links.car.href)}>Delete</button>
+            )
+        }
     ];
 
     if(!isSuccess) {
@@ -28,7 +52,9 @@ function CarList() {
             <DataGrid
                 rows={data}
                 columns={columns}
-                getRowId={row => row._links.self.href} />
+                disableRowSelectionOnClick={true}
+                getRowId={row => row._links.self.href} 
+            />
         );
     }
 }
