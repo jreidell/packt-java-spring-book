9a1d2765ec3abc55616a59259772d59a614c323e
joe.reidell <joe@reidell.net>
2024-03-30 17:28:41 -0400

    added Chapter16 folder and copied the CarDatabase and CarFront applications from the Chapter15 folder to the Chapter16 folder

---
+++ Chapter16/carfront/src/components/AddCar.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Dialog , DialogActions, DialogTitle, Tooltip } from "@mui/material";
+import { IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Cancel";
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import { Car } from "../Types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addCar } from "../api/carapi";
+import CarDialogContent from "./CarDialogContent";
+
+function AddCar() {
+    const [open, setOpen] = useState(false);
+    const [car, setCar] = useState<Car>({
+        brand: "",
+        model: "",
+        color: "",
+        registrationNumber: "",
+        modelYear: 0,
+        price: 0
+    });
+
+    const queryClient = useQueryClient();
+
+    const { mutate } = useMutation(addCar, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["cars"]);
+        },
+        onError: (err) => {
+            console.error(err);
+        }
+    });
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCar({...car, [event.target.name]: event.target.value});
+    };
+
+    const handleSave = () => {
+        mutate(car);
+        setCar({
+            brand: "",
+            model: "",
+            color: "",
+            registrationNumber: "",
+            modelYear: 0,
+            price: 0
+        });
+        handleClose();
+    };
+
+    return(
+        <>
+        <Tooltip title="Add New Car">
+        <IconButton onClick={handleClickOpen}><AddIcon fontSize="medium" /><DirectionsCarIcon fontSize="medium" /></IconButton>
+        </Tooltip>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>New Car</DialogTitle>
+            <CarDialogContent car={car} handleChange={handleChange} />
+            <DialogActions>
+            <Tooltip title="Cancel">
+                    <CancelIcon fontSize="medium" onClick={handleClose}>Cancel</CancelIcon>
+                </Tooltip>
+                <Tooltip title="Save">
+                    <SaveIcon fontSize="medium" onClick={handleSave}>Save</SaveIcon>
+                </Tooltip>
+            </DialogActions>
+        </Dialog>
+        </>
+    );
+}
+
+export default AddCar;
